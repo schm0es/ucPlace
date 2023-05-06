2e6b18a41ac9110acb2d62a06bf69c201d75bd1f
schm0es <87854942+schm0es@users.noreply.github.com>
2023-05-05 22:14:22 -0700

    it work
    kinda

--- front/app.py
+++ front/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, render_template, jsonify, redirect, url_for, session
-from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
 from flask_cors import CORS
 
 from sqlalchemy.orm import sessionmaker
 from flask_sqlalchemy import SQLAlchemy
-import db
+import db 
 from flask_admin.contrib.sqla import ModelView
-from werkzeug.security import generate_password_hash, check_password_hash
-
+from werkzeug.security import generate_password_hash
 
 Session = sessionmaker(bind=db.engine)
 db_session = Session()
@@ -44,7 +43,8 @@ def get_pix():
 
 @app.route('/pixels/<id>/<x>/<y>/<color>', methods=['POST'])
 def write_pix(id, x, y, color):
-    new_pixel = db.Pixel(id=id, x=x, y=y, color=color)
+    logged_user_id = current_user.get_id()
+    new_pixel = db.Pixel(id=logged_user_id, x=x, y=y, color=color)
     db_session.add(new_pixel)
     db_session.commit()
     return jsonify(message='Pixel successfully stored', success=True)
@@ -54,7 +54,8 @@ def update_pix(id, x, y, color):
     pixel_to_update = db_session.query(db.Pixel).filter(db.Pixel.x == x, db.Pixel.y == y).first()
 
     if pixel_to_update:
-        pixel_to_update.id = id
+        logged_user_id = current_user.get_id()
+        pixel_to_update.id = logged_user_id
         pixel_to_update.color = color
         db_session.commit()
         return jsonify({id: color})
@@ -68,9 +69,11 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
+        user = db_session.query(db.User).filter(db.User.username == username).first()
+
         # Replace this with your own logic to check if the username and password are correct
-        if (username == 'bob' and password == 'lol') or (username == 'johnsmith' and password == 'password123'):
-            user = User(username)
+        if user is not None and user.check_password(password):
+            print(user)
             login_user(user)
             session['logged_in'] = True
             return redirect(url_for('load_app'))
@@ -78,6 +81,28 @@ def login():
             return render_template('login.html', message='Invalid username or password')
 
     return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user_check = db_session.query(db.User).filter(db.User.username == username).first()
+
+        # Replace this with your own logic to check if the username and password are correct
+        if user_check is None:
+            new_user = db.User(username = username, password_hash = generate_password_hash(password))
+            db_session.add(new_user)
+            db_session.commit()
+            login_user(new_user)
+            return redirect(url_for('load_app'))
+        else:
+            return render_template('login.html', message='Invalid username or password')
+
+    return render_template('signup.html')
+
 
 @login_required
 @app.route("/app")
@@ -90,6 +115,14 @@ def logout():
     logout_user()
     session.pop('logged_in', None)
     return redirect(url_for('login'))
+
+@app.route('/get_user_id', methods=['GET'])
+@login_required
+def get_user_id():
+    userId=current_user.get_id()
+    print(userId)
+    return userId
+
 
 if __name__ == "__main__":
     app.run(debug=True)
